cfde750aecac144679d156fa5715fe53944e7b26
Byeongsoo <happyname0617@gmail.com>
2017-07-27 22:16:23 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+
+Simple Cashflow for Private Person
+
+Welcome to CashFlowP
+
+
+## How to launch the app from the Terminal:
+
+    $ node app.js
+
+https://cashflow-happyname0617.c9users.io
---
+++ app.js
@@ -0,0 +1,369 @@
+var env = process.env.NODE_ENV || 'development';
+var config = require('./config')[env];
+//var dictionary = require('./dictionary');
+var moment = require('moment');
+const express = require('express')
+var session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+var winston = require('winston');
+var async = require("async");
+
+var app = express()
+var request = require('request');
+var VError = require('verror');
+var fs = require('fs')
+var _ = require('underscore');
+var bodyParser = require('body-parser');
+
+var MongoClient = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectID;
+var assert = require('assert');
+
+
+var passport = require('passport')
+var FacebookStrategy = require('passport-facebook').Strategy;
+var GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+app.use(express.static('client'))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+app.use(session({
+  secret: config.SESSION_SECRET_KEY,
+  resave: false,
+  saveUninitialized: true,
+  store:new MongoStore({ url: config.DB_URL })
+}));
+
+var logger = new (winston.Logger)({
+  transports: [
+    new (winston.transports.Console)({
+      timestamp: true
+    })
+  ]
+});
+var mongodb;
+var collectionUser;
+var collectionList;
+function dbconnect(url,callback)
+{
+
+  logger.info("DB connect")
+  MongoClient.connect(url, function(err, db) {
+     if (err) { 
+        var error = new VError(err,"DB connect %s", url) 
+        logger.error(error);
+        callback(error); 
+     }
+    logger.info("Connected correctly to server");
+    mongodb=db;
+    collectionUser = mongodb.collection('User');
+    collectionList = mongodb.collection('Wishlist');
+    callback();
+  });
+}
+
+function dbdisconnect(){
+  mongodb.close();
+}
+
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new GoogleStrategy({
+    clientID: config.GOOGLE_CLIENT_ID,
+    clientSecret: config.GOOGLE_CLIENT_SECRET,
+    callbackURL: config.GOOGLE_CALLBACK_URL
+  },
+  function(accessToken, refreshToken, profile, done) {
+    logger.info(profile.name)
+    var _id = 'google:'+profile.id;;
+      collectionUser.findOne({_id:_id},function(err, foundUser) {
+        if (err) {logger.error('google strategy:',err); return done(err); }
+        if(foundUser){
+          logger.info("user already exist, proceed to login")
+          done(null, foundUser);
+        }
+        else
+        {
+          logger.info("new user, proceed to login")
+          var newuser = profile._json;
+          newuser._id = _id;
+          newuser.displayName = JSON.stringify(profile._json.name);
+          logger.info('new user displayName',newuser.displayName)
+
+          collectionUser.insertOne(newuser,function(err, newuser) {
+            if (err) {var newError = VError(err,'google strategy InsertOne'); return done(newError); }
+	        });
+          done(null,newuser);
+        }
+      });
+  }
+));
+
+passport.use(
+  new FacebookStrategy({
+    clientID: config.FACEBOOK_APP_ID,
+    clientSecret: config.FACEBOOK_APP_SECRET,
+    callbackURL: config.FACEBOOK_CALLBACK_URL,
+    profileFields:['id', 'email', 'gender', 'link', 'locale', 'name', 'timezone', 'verified', 'displayName','age_range']
+  },
+  function(accessToken, refreshToken, profile, done) {
+    logger.info('facebook login:',profile.name);
+    var _id = 'facebook:'+profile.id;;
+      collectionUser.findOne({_id:_id},function(err, foundUser) {
+        if (err) {logger.error(err); return done(err); }
+        if(foundUser){
+          logger.info("user already exist, proceed to login")
+          done(null, foundUser);
+        }
+        else
+        {
+          logger.info("new user, proceed to login")
+          var newuser = profile._json;
+          newuser._id = _id;
+          newuser.displayName = profile._json.name;
+          collectionUser.insertOne(newuser,function(err, newuser) {
+            if (err) {var newError = VError(err,'facebook strategy insertOne'); return done(newError); }
+	        });
+          done(null,newuser);
+        }
+      });
+  }
+
+));
+
+
+passport.serializeUser(function(user, done) {
+  logger.info('         serializeUser',user._id, user.name);
+  done(null, user._id);
+});
+
+passport.deserializeUser(function(id, done) {
+ collectionUser.findOne({_id:id},function(err, foundUser) {
+        if(err){logger.error('deserializeUser',err); return done(err);}
+        if(foundUser)
+        {
+          logger.info('         deserializeUser ', id, foundUser.name)
+          done(err, foundUser);
+        }
+        else
+        {
+          done('not existing FoundUser');
+        }
+  });
+
+});
+
+
+function initialize(){
+  dbconnect(config.DB_URL,function(err){
+    if (err) { var newError = new VError(err,'initialize dbconnect');  return newError; }
+
+  });
+}
+
+initialize();
+
+
+app.get('/auth/logout', function(req, res){
+  if(req.user)
+  {
+    logger.info('/auth/logout', req.user._id, req.user.name);
+    req.logout();
+    req.session.save(function(){
+      res.redirect('/');
+    });
+  }
+  else{
+    logger.info('/auth/logout not logged in approach')
+    res.redirect('/');
+  }
+});
+
+
+
+
+// Redirect the user to Facebook for authentication.  When complete,
+// Facebook will redirect the user back to the application at
+//     /auth/facebook/callback
+app.get('/auth/facebook', passport.authenticate('facebook',{scope:'email'}));
+
+// Facebook will redirect the user to this URL after approval.  Finish the
+// authentication process by attempting to obtain an access token.  If
+// access was granted, the user will be logged in.  Otherwise,
+// authentication has failed.
+app.get('/auth/facebook/callback',
+  passport.authenticate('facebook', { successRedirect: '/',
+                                      failureRedirect: '/' }));
+
+
+app.get('/auth/google',passport.authenticate('google', { scope: ['profile','email'] }));
+
+app.get('/auth/google/callback', 
+  passport.authenticate('google', { successRedirect:'/',
+                                    failureRedirect: '/' }));
+
+var TERM = {monthly:1,quaterly:3,yearly:12};
+// function getNextAvailableDate(startDateTimeStamp,repeatdate,repeatterm)
+// {
+//   var startDate = new Date(startDateTimeStamp)
+//   var month = 
+//   var firstCandidate = new Date(startDate.getFullYear(), month, repeatdate,
+//           startDate.getHours(), startDate.getMinutes(), startDate.getSeconds(), startDate.getMilliseconds());
+  
+//   if(firstCandidate>startDate)
+//   {
+//     return firstCandidate;
+//   }
+//   else
+//   {
+//     var m = moment(firstCandidate);
+//     m.add(TERM[repeatterm],'months');
+//     return m.toDate();
+//   }
+// }
+
+function addToList(startDate, endDate, dayList,item){
+  if(item.repeat)
+  {
+    var initDate = new Date(item.startYear,item.startMonth,item.repeatdate,0,0,0,0);
+    console.log('/addToList item.title',item.title)
+    console.log('/addToList item.repeat',item.repeat)
+    console.log('/addToList initDate ',initDate)
+    console.log('/addToList endDate ',endDate)
+    console.log('/addToList repeatdate ',item.repeatdate)
+    console.log('/addToList repeatterm ',item.repeatterm)
+    var candidate = initDate;
+
+    while(candidate<endDate && candidate>startDate)
+    {
+      dayList[candidate].push(item);
+      console.log('/addToList candidate',candidate)
+      var m = moment(candidate);
+      m.add(TERM[item.repeatterm],'months');
+      candidate = m.toDate();;
+    }
+  }
+  else{
+    var mydate = new Date(item.year,item.month,item.date,0,0,0,0);
+    if(dayList[mydate])
+    {
+      dayList[mydate].push(item);
+    }
+  } 
+  return dayList;
+}
+app.get('/',function(req,res){
+  // if(req.user) //logged in user?
+  // {
+  var list = [
+    {title:'집세',price:795,type:'expense',repeat:true,repeatterm:'monthly',repeatdate:1,startYear:2017,startMonth:7},
+    {title:'집주차비',price:36,type:'expense',repeat:true,repeatterm:'monthly',repeatdate:1,startYear:2017,startMonth:7},
+    {title:'생활비',price:500,type:'expense',repeat:true,repeatterm:'monthly',repeatdate:1,startYear:2017,startMonth:7},
+    {title:'병수사무실',price:250,type:'expense',repeat:true,repeatterm:'quaterly',repeatdate:7,startYear:2017,startMonth:7},
+    {title:'요셉유치원',price:94,type:'expense',repeat:true,repeatterm:'monthly',repeatdate:5,startYear:2017,startMonth:7},
+    {title:'ARD',price:52.5,type:'expense',repeat:true,repeatterm:'quaterly',repeatdate:10,startYear:2017,startMonth:7},
+    {title:'HASPA KONTO',price:7.9,type:'expense',repeat:true,repeatterm:'monthly',repeatdate:28,startYear:2017,startMonth:6},
+    {title:'통신비',price:100,type:'expense',repeat:true,repeatterm:'monthly',repeatdate:15,startYear:2017,startMonth:7},
+    {title:'기름값 및 주차비',price:150,type:'expense',repeat:true,repeatterm:'monthly',repeatdate:15,startYear:2017,startMonth:7},
+    {title:'차량유지비',price:50,type:'expense',repeat:true,repeatterm:'monthly',repeatdate:26,startYear:2017,startMonth:7},
+    {title:'차보험',price:280,type:'expense',repeat:true,repeatterm:'quaterly',repeatdate:3,startYear:2017,startMonth:9},
+    {title:'전기세',price:52,type:'expense',repeat:true,repeatterm:'monthly',repeatdate:19,startYear:2017,startMonth:7},
+    {title:'재림용돈',price:100,type:'expense',repeat:true,repeatterm:'monthly',repeatdate:2,startYear:2017,startMonth:7},
+    {title:'병수용돈',price:100,type:'expense',repeat:true,repeatterm:'monthly',repeatdate:2,startYear:2017,startMonth:7},
+    {title:'병수회사',price:100,type:'expense',repeat:true,repeatterm:'monthly',repeatdate:2,startYear:2017,startMonth:7},
+    {title:'KinderGeld',price:100,type:'income',repeat:true,repeatterm:'monthly',repeatdate:20,startYear:2017,startMonth:7},
+    {title:'ALG',price:2070,type:'income',repeat:true,repeatterm:'monthly',repeatdate:27,startYear:2017,startMonth:9},
+    {title:'ALG',price:550,type:'income',repeat:false,date:27,year:2017,month:8}
+    ];
+  var initBalance = 5618;
+  var initDate = new Date(2017,6,27,0,0,0,0);//remove hours,minitue,seconds
+  var duration = 210;
+  var dailyList ={};
+  console.log('initDate',initDate)
+
+  var m = moment(initDate);
+  m.add(duration,'days');
+  var endDate = m.toDate();;
+  console.log('endDate',endDate)
+
+  //initialize array
+  for(var i=0;i<duration;i++)
+  {
+    var m = moment(initDate);
+    m.add(i,'days');
+    var newDayTimeStamp = m.toDate();
+    console.log('newDayTimeStamp',newDayTimeStamp);
+    dailyList[newDayTimeStamp]=[]; 
+  }
+  
+  for(var i=0;i<list.length;i++)
+  {
+    console.log('addToList',i);
+    dailyList = addToList(initDate,endDate,dailyList,list[i]);
+  }
+
+  var mylist =[];
+  var prevBalance = initBalance;
+  for(var key in dailyList)
+  {
+    console.log(key,dailyList[key])
+    var date = new Date(key).toLocaleDateString()
+    var income = dailyList[key].reduce(
+      function(acc,item){
+        if(item.type=='income')
+        {
+          return acc+item.price;
+        }else
+        {
+          return acc;
+        }
+    },0);
+    var expense = dailyList[key].reduce(
+      function(acc,item){
+        if(item.type=='expense')
+        {
+          return acc+item.price;
+        }else
+        {
+          return acc;
+        }
+    },0);
+    var title = dailyList[key].reduce(
+      function(acc,item){
+          return acc+'|'+item.title;
+    },'');
+
+    var balance = prevBalance - expense + income;
+    prevBalance = balance;
+    var netbalance = balance ;
+    var dayItem = {date:date,title:title,income:income,expense:expense,balance:balance,netbalance:netbalance};
+    mylist.push(dayItem);
+  }
+  // for(var i=0;i<duration;i++)
+  // {
+  //   var date = today + i*(24*60*60*1000);
+  //   var title
+  //   dailyList.push({date:date}); 
+  // }
+  // var mylist = [
+  //   {date:'2017년7월27일',title:'집세',income:0,expense:795,balance:'3409',netbalance:'2290'},
+  //   {date:'2017년7월27일',title:'집세',income:0,expense:795,balance:'3409',netbalance:'2290'},
+  //   {date:'2017년7월27일',title:'집세',income:0,expense:795,balance:'3409',netbalance:'2290'}
+  //   ];
+    
+    res.render('home_login',{list:list,dailyList:mylist});
+  // }
+  // else{
+  //   res.render('landingpage');
+    
+  // }
+})
+
+app.listen(process.env.PORT || config.NODE_PORT, process.env.IP || "0.0.0.0",function(){
+  logger.info('listening on %s',process.env.PORT||config.NODE_PORT)
+})
